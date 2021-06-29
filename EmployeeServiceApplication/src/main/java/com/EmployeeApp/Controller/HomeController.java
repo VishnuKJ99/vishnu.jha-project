760054888fd26a8596b55d9ebe28ac7098ce98ad
Vishnu <Vishnu@LAPTOP-LK6MTT98>
2021-06-29 14:55:48 +0530

    Implementing Rest Controller with GET and POST

--- EmployeeServiceApplication/src/main/java/com/EmployeeApp/Controller/HomeController.java
+++ EmployeeServiceApplication/src/main/java/com/EmployeeApp/Controller/HomeController.java
@@ -1,37 +1,41 @@
 package com.EmployeeApp.Controller;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.EmployeeApp.DTO.EmployeeDTO;
+import com.EmployeeApp.Entity.Employee;
 import com.EmployeeApp.Service.EmployeeService;
 
-@Controller
+@RestController
+@CrossOrigin
 public class HomeController {
 	
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@RequestMapping("/Home")
-	public String home() {
-		return "Register.jsp";
+	@PostMapping("/registerEmployee")
+	@Transactional
+	public String registerEmployee(@RequestBody EmployeeDTO employee) {
+		employeeService.insertEmployee(employee);
+		
+		return "Employee successfully registered";
+	}
+	
+	@GetMapping("/displayEmployee")
+	public Iterable<Employee> displayEmployee(){
+		return employeeService.displayEmployee();
 		
 	}
-	
-	@RequestMapping("getData")
-	public ModelAndView getData(EmployeeDTO employeeDTO) {
-		ModelAndView mv=new ModelAndView();
-		mv.addObject("employeeDTO",employeeDTO);
-		mv.setViewName("Home.jsp");
-		employeeService.insertEmployee(employeeDTO);
-		System.out.println(employeeService.displayEmployee());
-		return mv;
-	}
-	
-//	public void displayAllEmployee() {
-//		employeeService.displayEmployee();
-//	}
 }
